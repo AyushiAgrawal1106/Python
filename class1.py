9af7c1e95fa726dc09349b59fdde26d8d39c7909
hj886 <ayushiagrwl11@gmail.com>
2024-03-09 10:40:54 +0530

    Python_First_Commit

---
+++ class1.py
@@ -0,0 +1,25 @@
+#a=26
+
+#count=0
+#for i in range(1,a+1):
+   # if(a%i==0):
+        #count=count+1
+
+#if(count==2):
+   # print("Prime number")
+#else:
+#    print("Not a prime number")
+
+#if(a&1==0):
+#    print("Even")
+#else:
+#    print("Odd")
+
+a=0
+b=1
+sum=0
+for i in range(1,11):
+    print(b)
+    sum=a+b
+    a=b
+    b=sum
